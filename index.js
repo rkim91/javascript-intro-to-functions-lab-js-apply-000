81b82efb9bde0fc6bd1ba68df8aa7c29aa61e3b4
Richard Kim <richeekim@gmail.com>
2018-01-24 01:03:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,20 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
+<<<<<<< HEAD
   if (string.toLowerCase() == string) {
     return "I can't hear you!"}
   else if (string.toUpperCase() == string) {
     return "YES INDEED!"}
   else if (string == string) {
     return "I love you, too."}
+=======
+  if (string.toLowerCase === "hello") {
+    return "I can\'t hear you!"}
+  else if (string.toUpperCase === "HELLO") {
+    return "YES INDEED!"}
+  else (string === "I love you, grandma.") {
+    return "I love you, too."
+}
+>>>>>>> f5524c591ad51a2ea1682be4c04496c04d5b01c9
 }
